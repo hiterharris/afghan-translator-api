28d94a86959136574f3404e2d4b1692740aec55e
Henry Harris IV <hiterharris@comcast.net>
2023-08-04 09:25:03 -0400

    remove key log

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
 const apiKey = process.env.OPENAI_API_KEY;
-console.log(apiKey);
 const configuration = new Configuration({ apiKey });
 const openai = new OpenAIApi(configuration);
 const config = { configuration, openai };
