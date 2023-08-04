2f3294c1ad89b7c7c726e6324620e08e6cd92110
root <nightfalcondev@gmail.com>
2023-08-04 13:56:06 +0000

    add headers

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const app = require("./server");
 
 const port = process.env.PORT || 3001;
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
