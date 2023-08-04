b43f29594d54a976bcbd737a02d97b50124b2f60
Henry Harris IV <hiterharris@comcast.net>
2023-08-04 09:23:51 -0400

    move cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require("./server");
+const cors = require("cors");
 
 const port = process.env.PORT || 3001;
+app.use(cors());
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const helmet = require("helmet");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const server = express();
 
@@ -11,7 +10,6 @@ const TranslateRouter = require("./routes/translate.js");
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
 server.use(bodyParser.json());
 
 server.get("/", (req, res) => {
