6b061a07c9e7f5516e2f47292f4c6cd8c08d93c8
hiterharris <hiterharris@comcast.net>
2023-08-21 19:32:29 -0400

    /support endpoint sending email

--- routes/support.js
+++ routes/support.js
@@ -1,42 +1,49 @@
+require('dotenv').config();
 const router = require("express").Router();
-const config = require("../config");
-const { configuration } = config;
-
-router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+const nodemailer = require("nodemailer");
 
 router.get("/", (req, res) => {
-    console.log('Support')
-    const response = {
-        message: "Support"
-    }
-    res.send(response);
-    // res.status(200).json("Support");
-  });
+    res.send("Afghan Translator Support");
+});
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
 
-
-    try {
-        // res.status(200).json(result);
-    } catch (error) {
-        if (error.response) {
-            console.error(error.response.status, error.response.data);
-            res.status(error.response.status).json(error.response.data);
-        } else {
-            console.error(`Error with email request: ${error.message}`);
-            res.status(500).json({
-                error: {
-                    message: 'An error occurred during your request.',
-                }
-            });
+    const transporter = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: process.env.MAILTRAP_USER,
+            pass: process.env.MAILTRAP_PASS
         }
-    }
+    });
+    
+    transporter.verify((error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Ready to Send");
+        }
+    });
+
+    const mail = {
+        from: name,
+        to: process.env.EMAIL_USER,
+        subject: "SUPPORT REQUEST - Afghan Translator",
+        html: `<p>Name: ${name}</p>
+             <p>Email: ${email}</p>
+             <p>Message: ${message}</p>`,
+    };
+
+    transporter.sendMail(mail, (error) => {
+        if (error) {
+            res.json({ status: "ERROR" });
+        } else {
+            res.json({ status: "Message Sent", mail });
+        }
+    });
 });
 
 module.exports = router;
