50db1591c4f7f745cd2c44ba48791d2b4cd4670e
hiterharris <hiterharris@comcast.net>
2023-08-22 09:05:15 -0400

    add headers to support

--- routes/support.js
+++ routes/support.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const router = require("express").Router();
 const nodemailer = require("nodemailer");
 
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 router.get("/", (req, res) => {
     res.send("Afghan Translator Support");
 });
