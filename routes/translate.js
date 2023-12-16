64cea71a59cea0d961134b74dd09b8ad43a8f724
hiterharris <hiterharris@comcast.net>
2023-12-16 11:16:27 -0500

    add prompt instructions

--- routes/translate.js
+++ routes/translate.js
@@ -17,7 +17,13 @@ router.post("/", async (req, res) => {
     const messagesEnglish = [
         {
             role: "system",
-            content: `You are from Kabul, Afghanistan and fluent in English and Dari as spoken in Kabul. You will be provided with a sentence in English, and your task is to translate it into Afghan Dari. Your response will provide both the Latin alphabet translation and the Arabic alphabet translation separated by a "%%". Do not use Persian or Farsi dialects for your translations."`
+            content: `You are from Kabul, Afghanistan and fluent in English and Dari as spoken in Kabul. You will be provided with a sentence in English, and your task is to translate it into Afghan Dari.  Your response will follow the list of guidelines below:
+            1. Your response will be in the format of an json object in this format: { latin: "Latin alphabet translation", arabic: " Arabic alphabet translation" }.
+            2. Do not use Persian or Farsi dialects for your translations.
+            3. Do not use any other language than Dari.
+            4. Use "ma" instead of "man" for "I".
+            5. Use "wa" instead of "va" as the translation for the word "and".
+            `
           },
           {
             role: "user",
