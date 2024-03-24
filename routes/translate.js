4b329c44efd23a4d8439eb20ee4953c7c5b2a991
hiterharris <hiterharris@comcast.net>
2024-03-24 17:47:28 -0400

    return object for dari to english

--- routes/translate.js
+++ routes/translate.js
@@ -18,7 +18,7 @@ router.post("/", async (req, res) => {
         {
             role: "system",
             content: `You are from Kabul, Afghanistan and fluent in English and Dari as spoken in Kabul. You will be provided with a sentence in English, and your task is to translate it into Afghan Dari.  Your response will follow the list of guidelines below:
-            1. Your response will be in the format of an json object in this format: { latin: "Latin alphabet translation", arabic: " Arabic alphabet translation" }.
+            1. Your response will be in the format of an json object in this format: { latin: "Latin alphabet translation", arabic: "Arabic alphabet translation" }.
             2. Do not use Persian or Farsi dialects for your translations.
             3. Do not use any other language than Dari.
             4. Use "ma" instead of "man" for "I".
@@ -34,7 +34,9 @@ router.post("/", async (req, res) => {
     const messagesDari = [
         {
             role: "system",
-            content: `You are from Kabul, Afghanistan and fluent in English and Dari as spoken in Kabul. You will be provided with a sentence in Dari, and your task is to translate it into American English. Your response will provide only the Latin alphabet translation. Do not use Persian or Farsi dialects for your translations."`
+            content: `You are from Kabul, Afghanistan and fluent in English and Dari as spoken in Kabul. You will be provided with a sentence in Dari, and your task is to translate it into American English. Your response will provide only the Latin alphabet translation. Do not use Persian or Farsi dialects for your translations. Your response will follow the list of guidelines below:
+            1. Your response will be in the format of an json object in this format: { latin: "Latin alphabet translation" }.
+            "`
         },
         {
             role: "user",
