6cbd18678ad224412e2941b71c20dc3a40225b67
hiterharris <hiterharris@comcast.net>
2024-04-30 13:36:45 -0400

    clean up status monitor use

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const statusMonitor = require('express-status-monitor')();
 const TranslateRouter = require("./routes/translate.js");
 const SupportRouter = require("./routes/support.js");
 
-server.use(require('express-status-monitor')({
-  title: 'Afghan Translator Status'
-}));
-server.get('/status', statusMonitor.pageRoute)
+server.use(require('express-status-monitor')({ title: 'Afghan Translator API' }));
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use(bodyParser.json());
+
+server.get('/status', statusMonitor.pageRoute);
 
 server.get("/", (req, res) => {
   res.send("Dari Translator API");
