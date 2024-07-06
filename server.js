75e3b047160972cab9abdc294e9a7801bc357c9d
hiterharris <hiterharris@comcast.net>
2024-07-06 09:04:04 -0400

    fix synk vulnerabilities

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const statusMonitor = require('express-status-monitor')();
 const server = express();
 const moesif = require('moesif-nodejs');
 
 const TranslateRouter = require("./routes/translate.js");
-const SupportRouter = require("./routes/support.js");
 const LogRouter = require("./routes/logs.js");
 
-server.use(require('express-status-monitor')({ title: 'Afghan Translator API' }));
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
@@ -25,14 +22,11 @@ const moesifMiddleware = moesif({
 
 server.use(moesifMiddleware);
 
-server.get('/status', statusMonitor.pageRoute);
-
 server.get("/", (req, res) => {
   res.send("Dari Translator API");
 });
 
 server.use("/api/translate", TranslateRouter);
-server.use("/api/support", SupportRouter);
 server.use("/api/logs", LogRouter);
 
 module.exports = server;
