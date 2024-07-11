7ef0234ff0848c66410184d6739a00926d2d3454
hiterharris <hiterharris@comcast.net>
2024-07-11 11:46:21 -0400

    add port to .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./server");
 const { logger } = require("./middleware");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   logger.info(`\n\nAfghan Translator API v1.3 \n\nServer listening on port ${port}`)
 });
