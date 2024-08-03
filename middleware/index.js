1ff8c9a5a3e54ef9606fe547ac6205678195993d
hiterharris <hiterharris@comcast.net>
2024-08-03 15:11:03 -0400

    move response.headers to middleware

--- middleware/index.js
+++ middleware/index.js
@@ -22,4 +22,12 @@ const checkRequestBody = (req, res, next) => {
     next();
 }
 
-module.exports = { logger, checkRequestBody };
+const responseHeaders = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+}
+
+module.exports = { logger, checkRequestBody, responseHeaders };
