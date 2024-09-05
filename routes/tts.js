2015dc224b4ee8d72ee0cc8b6518d986ca7427f0
hiterharris <hiterharris@comcast.net>
2024-09-04 23:12:35 -0400

    move key check

--- routes/tts.js
+++ routes/tts.js
@@ -27,15 +27,15 @@ const getVoices = async () => {
   }
 }
 
-if (!apiKey) {
-  res.status(500).json({
-    error: { message: "ElevenLabs key not configured" }
-  });
-  return;
-}
-
 router.post("/", async (req, res) => {
   const { text } = req.body;
+
+  if (!apiKey) {
+    res.status(500).json({
+      error: { message: "ElevenLabs key not configured" }
+    });
+    return;
+  }
 
   if (!text) {
     return res.status(400).json({ error: 'The "text" field is required.' });
